72e5c5c1d0e303c670e3bf6e2196a2e4568fb6ee
Shardion <22127191+Shardion@users.noreply.github.com>
2023-04-27 15:15:26 -0700

    many, many changes, including:
    systemd-resolved removal
    
    finally working system-wide dnscrypt-proxy2
    
    no more mesa hack
    
    bump

--- hardware/gpu.nix
+++ hardware/gpu.nix
@@ -5,7 +5,4 @@
   hardware.opengl.enable = true;
   hardware.opengl.driSupport = true;
   hardware.opengl.driSupport32Bit = true;
-
-  # Strange bug: default was bumped to `pkgs.mesa_23`, but `pkgs.mesa` refers to `pkgs.mesa_22`
-  hardware.opengl.mesaPackage = pkgs.mesa;
 }
--- system/universal/networking.nix
+++ system/universal/networking.nix
@@ -4,11 +4,6 @@
   # Set up Tailscale
   services.tailscale.enable = true;
 
-  services.resolved = {
-    enable = true;
-    domains = [ "tail354c3.ts.net" ];
-  };
-
   networking = {
     nameservers = [
       "100.100.100.100"
@@ -21,45 +16,28 @@
     firewall = {
       trustedInterfaces = [ "tailscale0" ];
     };
-    dhcpcd.extraConfig = "nohook resolv.conf";
     useDHCP = false;
+    resolvconf = {
+      useLocalResolver = true;
+    };
   };
 
-  # Set up system-wide DNS encryption
   services.dnscrypt-proxy2 = {
     enable = true;
     settings = {
-      ipv6_servers = true;
       require_dnssec = true;
       require_nolog = true;
       require_nofilter = false;
-      server_names = [ 
-        "quad9-dnscrypt-ip4-filter-pri"
-        "quad9-dnscrypt-ip4-filter-alt"
-        "quad9-dnscrypt-ip4-filter-alt2"
-        "quad9-dnscrypt-ip6-filter-pri"
-        "quad9-dnscrypt-ip6-filter-alt"
-        "quad9-dnscrypt-ip6-filter-alt2"
-      ];
+
+      listen_addresses = ["[::1]:53" "127.0.0.1:53"];
+      bootstrap_resolvers = ["[2620:fe::11]:53" "[2620:fe::fe:11]:53" "9.9.9.9:53" "149.112.112.11:53"];
+      netprobe_address = "9.9.9.9:53";
 
       sources.quad9-resolvers = {
-        urls = [
-          "https://www.quad9.net/quad9-resolvers.md"
-          "https://raw.githubusercontent.com/Quad9DNS/dnscrypt-settings/main/dnscrypt/quad9-resolvers.md"
-        ];
-        cache_file = "/var/lib/dnscrypt-proxy/quad9-resolvers.md";
+        urls = ["https://www.quad9.net/quad9-resolvers.md"];
         minisign_key = "RWQBphd2+f6eiAqBsvDZEBXBGHQBJfeG6G+wJPPKxCZMoEQYpmoysKUN";
+        cache_file = "/tmp/quad9-resolvers.md";
         prefix = "quad9-";
-        refresh_delay = 72;
-      };
-
-      sources.public-resolvers = {
-        urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-        ];
-        cache_file = "/var/lib/dnscrypt-proxy/public-resolvers.md";
-        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
       };
     };
   };
