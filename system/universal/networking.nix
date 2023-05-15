8a4b04737f9cf0aa5e0d50e63759c6dd2f5dd4bc
Shardion <22127191+Shardion@users.noreply.github.com>
2023-05-14 18:49:12 -0700

    trying something to fix tama

--- system/universal/networking.nix
+++ system/universal/networking.nix
@@ -2,9 +2,7 @@
 
 {
   networking = {
-    # NOTE: This option is deprecated, and set to false out of necessity;
-    # DHCP is instead set individually for each interface in its relevant `machines/` configuration.
-    useDHCP = false;
+    useDHCP = true;
   };
 
   # Configure some general Caddy settings
